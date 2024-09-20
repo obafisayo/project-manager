31ae9ef6af27c3fb2c5a649fbfef8303b6f626e7
obafisayo <abimbolaobafisayo@gmail.com>
2024-09-20 20:28:24 +0100

    commit changes errupted from merge conflicts

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -53,7 +53,10 @@ const Routes = () => {
           path: CREATE_PROJECTS,
           element: <CreateProjects />
         },
-
+        {
+          path: EDIT_PROJECTS,
+          element: <EditProject />
+        },
         {
           path: TASKS,
           element: <Task />
